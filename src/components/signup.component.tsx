4bfb599f48b2cb85bc3e36b40cddc6c73870ddcd
Diego Alvarado <diegoalvarado26@hotmail.com>
2020-03-24 17:24:31 -0300

    Added sign in

--- src/components/signup.component.tsx
+++ src/components/signup.component.tsx
@@ -3,9 +3,22 @@ import React, { Component  } from 'react';
 export default class SignUp extends Component {
     render(){
         return (
-            <div>
-                <h3>SignUp</h3>
-            </div>
+            <form>
+                <h3>Registrarse</h3>
+
+                <div className="form-group">
+                    <label> Ingrese el nombre de Barrio </label>
+                    <input type='text' className='form-control' placeholder='barrio'/>
+                </div>
+
+                <div className='form-group'>
+                    <label>Ingrese la contraseña</label>
+                    <input type='text' className='form-control' placeholder='Contraseña'/>
+                    <input type='text' className='form-control' placeholder='Ingrese nuevamente la contraseña'/>
+                </div>
+
+                <button type='submit'> Aceptar </button>
+            </form>
         );
     }
 }
