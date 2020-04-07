091ca424ac3e4aa3b7be5eb0a39e7bba347e48f0
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-04-06 23:28:37 -0300

    lote deletion and creation

--- src/screens/lote.screen.tsx
+++ src/screens/lote.screen.tsx
@@ -1,44 +1,103 @@
 import React, { useState, useEffect } from "react";
 import { withCookies, useCookies } from "react-cookie";
 import MaterialTable from "material-table";
-import Popup from 'reactjs-popup'
+import Popup from "reactjs-popup";
+import { resolve } from "dns";
 
 const axios = require("axios").default;
 
-var QRCode = require('qrcode.react');
+var QRCode = require("qrcode.react");
 
 function LoteScreen() {
-
-  const [cookie] = useCookies(['session'])
-  const [lotes, setLotes] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [showQR, setShowQR] = useState(false)
-  const [associationURL, setAssociationURL] = useState(null)
+  const [cookie] = useCookies(["session"]);
+  const [lotes, setLotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showQR, setShowQR] = useState(false);
+  const [associationURL, setAssociationURL] = useState(null);
 
   const actions = [
     {
-      icon: 'important_devices',
-      tooltip: 'Asociar Propietario',
+      icon: "important_devices",
+      tooltip: "Asociar Propietario",
       onClick: (event, rowData) => {
-        setAssociationURL(`lote/associate?lote=${rowData.lote_id}&barrio=${cookie.session.acc_id}`)
-        setShowQR(true)
-      }
-    }
-  ]
+        setAssociationURL(
+          `lote/associate?lote=${rowData.lote_id}&barrio=${cookie.session.acc_id}`
+        );
+        setShowQR(true);
+      },
+    },
+  ];
 
-  useEffect(() =>{
+  const edit = {
+    isEditable: (rowData) => false,
+    isDeletable: (rowData) => true,
+    onRowAdd: (newData) => {
+      let data = newData;
+      return new Promise((resolve, reject) => {
+        setTimeout(
+          () =>
+            axios({
+              method: "post",
+              url: "http://localhost:3500/lote/new",
+              data: {
+                name: newData.lote_name,
+                num: +newData.lote_num,
+                street: newData.lote_street,
+                code: +newData.lote_code,
+              },
+              headers: {
+                Authorization: cookie.session.token,
+              },
+            }).then((response) => {
+              if (response.data) {
+                data.propietarios = [];
+                setLotes([...lotes, data]);
+                resolve();
+              }
+              reject();
+            }),
+          2000
+        );
+      });
+    },
+    onRowDelete: (oldData) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          axios({
+            method: "delete",
+            url: "http://localhost:3500/lote/delete",
+            params: {
+              lote: oldData.lote_id
+            },
+            headers: {
+              Authorization: cookie.session.token,
+            },
+          }).then(response => {
+            if(response){
+              setLotes([...lotes.filter(lote => lote.lote_id != oldData.lote_id)])
+              resolve()
+            }
+            reject()
+          })
+        }, 2000);
+      });
+    },
+  };
+
+  useEffect(() => {
     axios({
-      method: 'get',
-      url: 'http://localhost:3500/lote/all',
-      headers:{
-        Authorization: cookie.session.token
-      }
-    }).then(response => {
-      setLotes(response.data)
-      setLoading(false)
+      method: "get",
+      url: "http://localhost:3500/lote/all",
+      headers: {
+        Authorization: cookie.session.token,
+      },
     })
-    .catch(error => console.error(error))
-  }, [])
+      .then((response) => {
+        setLotes(response.data);
+        setLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <React.Fragment>
@@ -51,29 +110,26 @@ function LoteScreen() {
         closeOnDocumentClick
         onClose={() => setShowQR(false)}
       >
-        <QRCode 
-        value={associationURL}
-        includeMargin={true}
-        size={512}
-        />
+        <QRCode value={associationURL} includeMargin={true} size={512} />
       </Popup>
       <MaterialTable
         title="Lista de Lotes"
+        editable={edit}
         actions={actions}
         isLoading={loading}
         columns={columns}
         data={lotes}
         detailPanel={[
           {
-          render: detailPanel
-        }]
-        }
+            render: detailPanel,
+          },
+        ]}
       />
     </React.Fragment>
   );
 }
 
-function detailPanel(rowData){
+function detailPanel(rowData) {
   return (
     <div>
       <h2>Propietarios</h2>
@@ -82,10 +138,12 @@ function detailPanel(rowData){
   );
 }
 
-function propietarioLabel(propietario){
+function propietarioLabel(propietario) {
   return (
     <div>
-      <h5>Nombre: {propietario.last_name}, {propietario.first_name}</h5>
+      <h5>
+        Nombre: {propietario.last_name}, {propietario.first_name}
+      </h5>
       <h5>DNI: {propietario.doc_id}</h5>
     </div>
   );
@@ -95,7 +153,7 @@ const columns = [
   { title: "Nombre/Número", field: "lote_name" },
   { title: "Calle", field: "lote_street" },
   { title: "Numero de Calle", field: "lote_num" },
-  { title: "Codigo Postal", field: "lote_code" }
-]
+  { title: "Codigo Postal", field: "lote_code" },
+];
 
 export default LoteScreen;
