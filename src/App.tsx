af1f48168f3a9fcbe369c6de0c30464df1319efd
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-04-15 17:03:20 -0300

    fixed qr and cookies

--- src/App.tsx
+++ src/App.tsx
@@ -4,61 +4,56 @@ import { useCookies } from "react-cookie";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavigationBar from "./components/navigation.bar";
-import LoginScreen from './screens/login.screen';
-import Table from './screens/lote.screen';
+import LoginScreen from "./screens/login.screen";
+import Table from "./screens/lote.screen";
 import PanelScreen from "./screens/panel.screen";
 import RegisterScreen from "./screens/register.screen";
 
-function App(){
+function App() {
+  const [cookie] = useCookies();
 
-  const [cookie] = useCookies(['session'])
+  const authenticated = !!cookie.session;
 
-  const authenticated = !!cookie.session
-
-  return ( 
+  return (
     <BrowserRouter>
-        <NavigationBar/>
-        <Switch>
-            <PrivateRoute path='/panel'>
-                <PanelScreen/>
-            </PrivateRoute>
-            <Route path='/login' component={LoginScreen}/>
-            <Route path='/register' component={RegisterScreen}/>
-            <Route path='/lotescreen' component={Table}/>
-            <Route path='/'>
-                {authenticated?
-                <Redirect to='/panel'/>
-                :
-                <Redirect to='/login'/>}
-            </Route>
-        </Switch>
+      <NavigationBar />
+      <Switch>
+        <PrivateRoute path="/panel">
+          <PanelScreen />
+        </PrivateRoute>
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/register" component={RegisterScreen} />
+        <Route path="/lotescreen" component={Table} />
+        <Route path="/">
+          {authenticated ? <Redirect to="/panel" /> : <Redirect to="/login" />}
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
-
 }
 
 function PrivateRoute({ children, ...rest }) {
-  const [cookie] = useCookies(['session'])
+  const [cookie] = useCookies();
 
-  const authenticated = !!cookie.session
+  const authenticated = !!cookie.session;
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-      authenticated ? (
+        authenticated ? (
           children
         ) : (
           <Redirect
             to={{
               pathname: "/login",
-              state: { from: location }
+              state: { from: location },
             }}
           />
         )
       }
     />
   );
-  }
+}
 
-export default App
+export default App;
--- src/screens/register.screen.tsx
+++ src/screens/register.screen.tsx
@@ -1,90 +1,128 @@
-import React, { useState  } from 'react';
-import { Redirect } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
+import { Redirect } from "react-router-dom";
 
-const axios = require('axios').default;
+const axios = require("axios").default;
 
-function RegisterScreen(){
+function RegisterScreen() {
+  const [cookie] = useCookies();
 
-    const [cookie] = useCookies(['session'])
+  const authenticated = !!cookie.session;
 
-    const authenticated = !!cookie.session
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [isRegistered, setIsRegistered] = useState(authenticated);
 
-    const [email, setEmail] = useState('')
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [message, setMessage] = useState('')
-    const [isRegistered, setIsRegistered] = useState(authenticated)
-
-
-    function handleSubmit(event){
-
-        if(isBlank(name)){
-            setMessage('Name is blank!')
-        }else if(isBlank(email)){
-            setMessage('Email is blank!')
-        }else if(isBlank(password)){
-            setMessage('Password is blank!')
-        }else if(password !== confirmPassword){
-            setMessage("Passwords don't match!")
-        }else{
-            axios({
-                method: 'post',
-                url: 'http://localhost:3500/barrio/register',
-                data:{email, name, password}
-            }).then(response => {
-                setIsRegistered(true)
-            }).catch(error => setMessage(`Error: ${error.message}`))
-        }
+  function handleSubmit(event) {
+    if (isBlank(name)) {
+      setMessage("Name is blank!");
+    } else if (isBlank(email)) {
+      setMessage("Email is blank!");
+    } else if (isBlank(password)) {
+      setMessage("Password is blank!");
+    } else if (password !== confirmPassword) {
+      setMessage("Passwords don't match!");
+    } else {
+      axios({
+        method: "post",
+        url: "http://localhost:3500/barrio/register",
+        data: { email, name, password },
+      })
+        .then((response) => {
+          setIsRegistered(true);
+        })
+        .catch((error) => setMessage(`Error: ${error.message}`));
     }
+  }
 
-        return (
-           <React.Fragment>
-            {isRegistered || authenticated ? 
-            <Redirect to='/login' />
-            :
-            <div style={{
-                position: 'absolute', left: '50%', top: '50%',
-            }}>
-                <h3>Registrarse</h3>
-                <label>{message}</label>
-                <div className="form-group">
-                    <label> Ingrese el nombre de Barrio 
-                    <input type='text' name='name' value={name} onChange={(event) => setName(event.target.value)} className='form-control' placeholder='' />
-                    </label>
-                </div>
+  return (
+    <React.Fragment>
+      {isRegistered || authenticated ? (
+        <Redirect to="/login" />
+      ) : (
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+          }}
+        >
+          <h3>Registrarse</h3>
+          <label>{message}</label>
+          <div className="form-group">
+            <label>
+              {" "}
+              Ingrese el nombre de Barrio
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                className="form-control"
+                placeholder=""
+              />
+            </label>
+          </div>
 
-                <div className="form-group">
-                    <label> Ingrese el correo electrónico del barrio 
-                    <input type='email' name='email' value={email} onChange={(event) => setEmail(event.target.value)} className='form-control' placeholder=''/>
-                    </label>
-                </div>
+          <div className="form-group">
+            <label>
+              {" "}
+              Ingrese el correo electrónico del barrio
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                className="form-control"
+                placeholder=""
+              />
+            </label>
+          </div>
 
-                <div className='form-group'>
-                    <label>Ingrese la contraseña
-                    <input type='password' name='password' value={password} onChange={(event) => setPassword(event.target.value)} className='form-control' placeholder=''/>
-                    </label>
-                </div>
+          <div className="form-group">
+            <label>
+              Ingrese la contraseña
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                className="form-control"
+                placeholder=""
+              />
+            </label>
+          </div>
 
-                <div>
-                    <label> Ingrese nuevamente la contraseña 
-                        <input type='password' name='confirm_password' value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} className='form-control' placeholder=''/>
-                    </label>
-                </div>
+          <div>
+            <label>
+              {" "}
+              Ingrese nuevamente la contraseña
+              <input
+                type="password"
+                name="confirm_password"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
+                className="form-control"
+                placeholder=""
+              />
+            </label>
+          </div>
 
-                <button type='button' onClick={handleSubmit}> Registrar </button>
-            </div>
-            }
-            </React.Fragment>
-        );
-    
+          <button type="button" onClick={handleSubmit}>
+            {" "}
+            Registrar{" "}
+          </button>
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
 function isBlank(str) {
-    return (!str || /^\s*$/.test(str));
+  return !str || /^\s*$/.test(str);
 }
 
-
-
-export default RegisterScreen
+export default RegisterScreen;
