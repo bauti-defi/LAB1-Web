549dcaa17634514930ae744a612405c57a49cf25
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2020-04-17 23:38:34 -0300

    new navbar, ugly and not done

--- src/screens/lotes/propietario.panel.tsx
+++ src/screens/lotes/propietario.panel.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { Lote, Propietario } from "../../storage/lotes.reducer";
+import { Lote } from "../../storage/lotes.reducer";
 
 function PropietariosPanel(rowData: Lote) {
   return (
     <div>
-      <h2>Propietarios</h2>
-      {rowData.propietarios.map(propietarioLabel)}
-    </div>
-  );
-}
-
-function propietarioLabel(propietario: Propietario) {
-  return (
-    <div key={propietario.id}>
-      <h5>
-        Nombre: {propietario.last_name}, {propietario.first_name}
-      </h5>
-      <h5>DNI: {propietario.doc_id}</h5>
+      <h2>Propietarios: {rowData.propietarios?.length}</h2>
     </div>
   );
 }
